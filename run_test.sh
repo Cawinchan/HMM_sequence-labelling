8f2b0d5f60fe1645cdd614c37c6f8e47f63a5374
Cawinchan <cawin_chan@gmail.com>
2021-12-09 18:54:58 +0800

    change to log probabilities for q1 and transmission, fixed emission paramter for unknown token

--- run_test.sh
+++ run_test.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
 
-echo "~~~~~~~~~~~~~~ Part 1: ES Results" 
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> results.txt
 
-python data/Eval/evalResult.py data/ES/dev.out data/ES/dev.p1.out
+echo "~~~~~~~~~~~~~~ Part 1: ES Results" >> results.txt
 
-echo "~~~~~~~~~~~~~~ Part 1: RU Results" 
+python data/Eval/evalResult.py data/ES/dev.out data/ES/dev.p1.out >> results.txt
 
-python data/Eval/evalResult.py data/RU/dev.out data/RU/dev.p1.out
+echo "~~~~~~~~~~~~~~ Part 1: RU Results" >> results.txt
 
-echo "~~~~~~~~~~~~~~ Part 2: ES Results" 
+python data/Eval/evalResult.py data/RU/dev.out data/RU/dev.p1.out >> results.txt
 
-python data/Eval/evalResult.py data/ES/dev.out data/ES/dev.p2.out
+echo "~~~~~~~~~~~~~~ Part 2: ES Results" >> results.txt
 
-echo "~~~~~~~~~~~~~~ Part 2: RU Results" 
+python data/Eval/evalResult.py data/ES/dev.out data/ES/dev.p2.out >> results.txt
 
-python data/Eval/evalResult.py data/RU/dev.out data/RU/dev.p2.out
+echo "~~~~~~~~~~~~~~ Part 2: RU Results" >> results.txt
 
-$SHELL
+python data/Eval/evalResult.py data/RU/dev.out data/RU/dev.p2.out >> results.txt
+    
